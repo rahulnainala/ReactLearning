7a3e3ab4195c3937f0ce83d0a99ed8a6dda4f8f4
formless <rahulnainala2@gmail.com>
2023-08-03 21:53:21 +0530

    Functional Component

--- (04) Laying Foundation/App.js
+++ (04) Laying Foundation/App.js
@@ -15,16 +15,38 @@ console.log(heading);
 //JSX is not HTML
 //JSX => Babel transpiles it to React.createElement => React Element (JS object) =>Rendered on DOM
 
+//JSX code :
 const jsxHeading = (
   <h1 className="head" tabIndex="5">
     Replaced the data using JSX
   </h1>
 );
 
-console.log(jsxHeading);
+//React Functional Component
+const Headingcomponent2 = () => {
+  return <h1 className="heading">Hello World using Functional Component</h1>;
+};
+
+const Title = () => <h2>React Code based on Babel</h2>;
+
+//Industry Standard
+//to call react element we use {} inside the component
+const Headingcomponent = () => (
+  <div id="container">
+    {jsxHeading}
+    {Title()}
+    <Title></Title>
+    <Title />
+    <h1 className="heading">Hello World using Functional Component</h1>
+  </div>
+);
+
+//console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //root.render(heading);
 
-root.render(jsxHeading);
+//root.render(jsxHeading);
+
+root.render(<Headingcomponent />);
